1e23b2dc621fa7e255fbebde72e7a3b2eda34e75
DULANA CHATHURMA <dulanachathurma99@gmail.com>
2025-09-20 21:52:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let speed=100;
+if(speed<80){
+    console.log("speed Up")
+    console.log("speed Up")
+    console.log("speed Up")
+}
+else{
+    console.log("speed Down")
+}
+
+console.log("program finished !")
