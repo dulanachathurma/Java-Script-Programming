c82b3db5f9dc6c170ca2485272f4dcbe3f71f260
DULANA CHATHURMA <dulanachathurma99@gmail.com>
2025-09-28 12:52:27 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,40 @@
+let marks = 90;
+console.log(" Hi Dulana!")
+if(marks>=85){
+    console.log(" A+")
+    console.log(" Your GPA = 4.0")
+}
+else if(marks>=70){
+    console.log(" A")
+    console.log(" Your GPA = 4.0")
+}
+else if(marks>=65){
+    console.log(" A-")
+    console.log(" Your GPA = 3.7")
+}
+else if(marks>=60){
+    console.log(" B+")
+    console.log(" Your GPA =3.3")
+}
+else if(marks>=55){
+    console.log("B")
+    console.log(" Your GPA = 3.0")
+}
+else if(marks>=50){
+    console.log(" B-")
+    console.log(" Your GPA = 2.7")
+}
+else if(marks>=45){
+    console.log(" C+")
+    console.log(" Your GPA = 2.3")
+}
+else if(marks>=40){
+    console.log(" C")
+    console.log(" Your GPA = 2.0")
+}
+else {
+    console.log(" Repeat")
+    console.log(" You should again face the Exam in next Year")
+    console.log(" Never Give Up")
+}
+ console.log(" program finished !")
